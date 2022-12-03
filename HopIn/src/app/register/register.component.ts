2cda587f2493039a4a54266ca6f2645d40918f81
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-04 00:45:32 +0100

    added secondary color theme

--- HopIn/src/app/register/register.component.ts
+++ HopIn/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'register',
