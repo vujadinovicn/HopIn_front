4c8c62d3555c6a2a1df7efef4b692627c4235438
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-03 13:19:08 +0100

    added form elements

--- HopIn/src/app/register/register.component.ts
+++ HopIn/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'register',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  registerForm = new FormGroup({
+    
+  })
 
   constructor() { }
 
