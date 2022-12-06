de56f7e3c87af199adeae0a10ca57fd247b8d82f
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-12-06 17:25:50 +0100

    added form group in typescript

--- HopIn/src/app/login/login.component.ts
+++ HopIn/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    if (this.loginForm.valid) {
+      console.log("Success");
+    }
   }
 
 }
