52c638ed8bce379e32be4cf90301e96655e55c6e
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-07 13:18:30 +0100

    added temorary routing

--- HopIn/src/infrastructure/app-routing.module.ts
+++ HopIn/src/infrastructure/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from 'src/app/landing/landing.component';
 import { LoginComponent } from 'src/app/login/login.component';
 import { RegisterComponent } from 'src/app/register/register.component';
 
 const routes: Routes = [
+  {path: '', component: LandingComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: LandingComponent}
 ];
 
 @NgModule({
