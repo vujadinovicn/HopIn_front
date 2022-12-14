6d28d90c58d0de8a1c84745509520fb61a2a820c
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-14 12:40:18 +0100

    added routing service and logic to now displat location entered in pickup-destination-form

--- HopIn/src/app/map/map.component.ts
+++ HopIn/src/app/map/map.component.ts
@@ -1,27 +1,37 @@
 /// <reference types="@types/google.maps" />
-import { Component, OnInit } from '@angular/core';
+
+import { RoutingService } from './../services/routing.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
   map : google.maps.Map = {} as google.maps.Map;
   pickup : google.maps.Marker = {} as google.maps.Marker;
   destination : google.maps.Marker = {} as google.maps.Marker;
 
-  constructor() { }
+  sub: Subscription;
+
+  constructor(private routingService: RoutingService) {
+    this.sub = this.routingService.receivedRoute().subscribe((route) => {
+      this.addMarker(route.pickup.lat, route.pickup.lng, 'Pickup');
+      this.addMarker(route.destination.lat, route.destination.lng, 'Destination');
+    });
+   }
 
   ngOnInit(): void {
     this.initMap();
-    this.addMarker(45.26247360000001, 19.8375849, 'Pickup');
-    this.addMarker(
-      45.2429669, 19.8531783, 'Destination');
+    console.log(this.pickup);
   }
 
-
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   initMap(): void {
     this.map = new google.maps.Map(
@@ -42,17 +52,28 @@ export class MapComponent implements OnInit {
 
   public addMarker(lat: number, lng: number, title: string) {
     let map = this.map;
-    let marker = new google.maps.Marker({
-      map,
-      draggable: true,
-      position: { lat: lat, lng: lng},
-      title: title
-    })
 
-    if (title == 'Pickup')
-      this.pickup = marker;
-    else 
-      this.destination = marker;
+    if (title == 'Pickup') {
+      if (Object.keys(this.pickup).length == 0) 
+        this.pickup = new google.maps.Marker({
+          map,
+          draggable: true,
+          position: { lat: lat, lng: lng},
+          title: title
+        });
+      else
+        this.pickup.setPosition({lat: lat, lng: lng});
+    } else {
+      if (Object.keys(this.destination).length == 0)
+      this.destination = new google.maps.Marker({
+        map,
+        draggable: true,
+        position: { lat: lat, lng: lng},
+        title: title
+      });
+      else
+        this.destination.setPosition({lat: lat, lng: lng});
+    }
       
   }
 
