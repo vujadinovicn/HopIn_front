99815c397f2b2095566a4359253c8ce40a0c8427
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-15 09:57:29 +0100

    enabled to choose empty scheduled time after choosing time in the first place

--- HopIn/src/app/validators/timeFormatValidator.ts
+++ HopIn/src/app/validators/timeFormatValidator.ts
@@ -2,8 +2,9 @@ import { AbstractControl } from '@angular/forms';
 
 export function timeFormatValidator( control: AbstractControl): { [key: string]: boolean } | null {
     const regex = /([01]?[0-9]|2[0-3]):[0-5][0-9]/;
-    if (control.value !== undefined && !regex.test(control.value)) {
-        return { timeFormatError: true };
-    }
+    if (control.dirty && control.value != '')
+        if (control.value !== undefined && !regex.test(control.value)) {
+            return { timeFormatError: true };
+        }
     return null
   }
