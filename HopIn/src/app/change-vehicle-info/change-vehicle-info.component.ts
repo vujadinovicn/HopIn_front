59f928b30db90c0887e0a38c77d238aa8ae343a8
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-12-21 02:50:12 +0100

    added bindings for checkboxes and buttons

--- HopIn/src/app/change-vehicle-info/change-vehicle-info.component.ts
+++ HopIn/src/app/change-vehicle-info/change-vehicle-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { markFormControlsTouched } from '../validators/formGroupValidators';
+import { modelRegexValidator, platesRegexValidator, seatsRegexValidator } from '../validators/vehicleValidator';
 
 @Component({
   selector: 'app-change-vehicle-info',
@@ -8,15 +10,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ChangeVehicleInfoComponent implements OnInit {
 
-  constructor() { }
+  vehicleType : string = "car";
+  isBabyTransport : boolean = false;
+  isPetTransport: boolean = false;
 
   vehicleInfoForm = new FormGroup({
-    model: new FormControl('', [Validators.required]),
-    plates: new FormControl('', [Validators.required]),
-    seats: new FormControl('', [Validators.required]),
+    model: new FormControl('', [Validators.required, modelRegexValidator]),
+    plates: new FormControl('', [Validators.required, platesRegexValidator]),
+    seats: new FormControl('', [Validators.required, seatsRegexValidator]),
   }, [])
 
+  constructor() { }
+
   ngOnInit(): void {
+    markFormControlsTouched(this.vehicleInfoForm);
   }
+
+  changeVehicleType(vehicleType: string) : void {
+    this.vehicleType = vehicleType;
+    console.log(this.vehicleType);
+  }
+
+  save() : void {}
 
 }
