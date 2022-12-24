dc51b32ceb2a8adb9864fe89ba301e0cc814757b
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-24 23:30:28 +0100

    added new methods in user service for communication with admin end points

--- HopIn/src/app/services/user.service.ts
+++ HopIn/src/app/services/user.service.ts
@@ -35,6 +35,10 @@ export class UserService {
     return this.http.get<User>(environment.apiHost + '/driver/' + driverId);
   }
 
+  getByAdminId(adminId: number): Observable<User> {
+    return this.http.get<User>(environment.apiHost + '/user/' + adminId);
+  }
+
   updateDriverPersonalInfo(driver: any): Observable<any> {
     const options: any = {
       responseType: 'json',
@@ -61,6 +65,20 @@ export class UserService {
       responseType: 'text',
     };
     return this.http.put<string>(environment.apiHost + '/passenger/1', passenger, options);
+  }
+
+  updateAdminPersonalInfo(admin: any): Observable<any> {
+    const options: any = {
+      responseType: 'json',
+    };
+    return this.http.put<string>(environment.apiHost + '/user/1', admin, options);
+  }
+
+  updateAdminPassword(admin: any): Observable<any> {
+    const options: any = {
+      responseType: 'text',
+    };
+    return this.http.put<string>(environment.apiHost + '/user/1', admin, options);
   }
 }
 
