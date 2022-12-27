fa18dd6d8aaee6f009ee40232f288c388ecb1129
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-27 12:58:28 +0100

    added attribute request in request feedback and binded data from attribute with html elements

--- HopIn/src/app/request-feedback/request-feedback.component.ts
+++ HopIn/src/app/request-feedback/request-feedback.component.ts
@@ -7,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestFeedbackComponent implements OnInit {
 
-  status: String = 'declined'
+  request: Request = {
+    status: 'declined',
+    reason: 'Lorem ipsum dolor sit amet consectetur. Sed metus risus amet at at tellus quis aenean. Euismod dui platea urna velit vel pellentesque egestas sed at. Interdum euismod auctor hendrerit maecenas elit. Lorem ipsum dolor sit amet consectetur.' 
+    };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface Request {
+  status: String;
+  reason: String;
 }
