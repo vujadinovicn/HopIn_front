1299e03eae6c8680dc7c53aa17c60f2c9c2628f0
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-28 16:02:12 +0100

    added getting pending request from backend and displaying them

---
+++ HopIn/src/app/services/driver-update-request.service.ts
@@ -0,0 +1,37 @@
+import { environment } from './../../environments/environment';
+import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { User } from "./user.service";
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UpdateRequestService {
+
+    constructor(private http: HttpClient) {}
+
+    getAllPending(): Observable<DriverAccountUpdateRequest[]> {
+        return this.http.get<DriverAccountUpdateRequest[]>(environment.apiHost + '/request/pending');
+    }
+
+    getAllPendingDriver(): Observable<DriverAccountUpdateRequest[]> {
+        return new Observable<DriverAccountUpdateRequest[]>();
+    }
+
+    getAllProcessedAdmin(): Observable<DriverAccountUpdateRequest[]> {
+        return this.http.get<DriverAccountUpdateRequest[]>(environment.apiHost + '/1/admin/processed');
+    }
+
+    getAllProcessedDriver(): Observable<DriverAccountUpdateRequest[]> {
+        return new Observable<DriverAccountUpdateRequest[]>();
+    }
+}
+
+export interface DriverAccountUpdateRequest {
+    id: number,
+    driver: User,
+    time: Date,
+    type: string,
+    status: string
+}
--- HopIn/src/app/update-requests-display/update-requests-display.component.ts
+++ HopIn/src/app/update-requests-display/update-requests-display.component.ts
@@ -1,3 +1,4 @@
+import { UpdateRequestService, DriverAccountUpdateRequest } from './../services/driver-update-request.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,20 +10,50 @@ export class UpdateRequestsDisplayComponent implements OnInit {
 
   currentOption: string = "pending";
 
-  requests: Request[] = [];
+  driverImgPath = "../../assets/vectors/login.svg";
 
-  constructor() { }
+  pendingRequests: DriverAccountUpdateRequest[] = [];
+  procesedRequests: DriverAccountUpdateRequest[] = [];
+  requests: DriverAccountUpdateRequest[] = [];
+
+  constructor(private service: UpdateRequestService) { }
 
   ngOnInit(): void {
+    this.getPending();
+    this.getProcessed();
   }
 
   public chooseOption(chosen: string) {
     this.currentOption = chosen;
     if (chosen == "pending") {
-      
+      this.requests = this.pendingRequests;
     } else {
-      
+      this.requests = this.procesedRequests;
     }
   }
 
+  private getPending() {
+    this.service.getAllPending().subscribe({
+      next: (result: DriverAccountUpdateRequest[]) => {
+        console.log(result);
+        this.pendingRequests = result;
+        this.requests = result;
+        this.requests[0].driver.profilePicture = this.driverImgPath;
+      },
+      error: (error: any) => {
+        alert("Error fetching pending requests :(");
+      }
+    });
+  }
+
+  private getProcessed() {
+    this.service.getAllProcessedAdmin().subscribe({
+      next: (result: DriverAccountUpdateRequest[]) => {
+        this.procesedRequests = result;
+      },
+      error: (error: any) => {
+        alert("Error fetching processed requests :(");
+      }
+    });
+  }
 }
