5fbe1ccf6c79584e2da0b9517d4e2754d46d1e13
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-29 01:58:21 +0100

    added bootstrapping different components depending on role in dahsboard

--- HopIn/src/app/driver-request-dashboard/driver-request-dashboard.component.ts
+++ HopIn/src/app/driver-request-dashboard/driver-request-dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverRequestDashboardComponent implements OnInit {
 
-  _role: String = 'driver';
+  _role: String = 'admin';
 
   constructor(private requestDetailsService: RequestDetailsService) { }
 
