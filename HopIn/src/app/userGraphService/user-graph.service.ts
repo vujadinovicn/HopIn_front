da1b86d8fc471ce7a11fa7f45df2936d4e59f94d
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-06 12:08:47 +0100

    changed endpoint methods in user-graph-service to receive user id

--- HopIn/src/app/userGraphService/user-graph.service.ts
+++ HopIn/src/app/userGraphService/user-graph.service.ts
@@ -14,12 +14,12 @@ export class UserGraphService {
 
   constructor(private http: HttpClient) {}
 
-  getAll(from: Dayjs, to:Dayjs, isDriver: boolean): Observable<RideForReport[]> {
-    if(isDriver) {
-      return this.http.get<RideForReport[]>(environment.apiHost + '/driver/2/ride/date?from=' +
+  getAll(from: Dayjs, to:Dayjs, role: String, id: number): Observable<RideForReport[]> {
+    if(role === 'ROLE_DRIVER') {
+      return this.http.get<RideForReport[]>(environment.apiHost + '/driver/' + id + '/ride/date?from=' +
       from.format('YYYY/MM/DD') + '&to=' + to.format('YYYY/MM/DD'));
     }
-    return this.http.get<RideForReport[]>(environment.apiHost + '/passenger/1/ride/date?from=' +
+    return this.http.get<RideForReport[]>(environment.apiHost + '/passenger/' + id + '/ride/date?from=' +
     from.format('YYYY/MM/DD') + '&to=' + to.format('YYYY/MM/DD'));
   }
 }
