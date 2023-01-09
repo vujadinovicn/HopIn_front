f96295e46d9cbd40777b27d59d42d210ec509d0a
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-09 22:34:58 +0100

    created methods for communication with backend apis for starting and ending count of working hours

---
+++ HopIn/src/app/services/working-hours.service.ts
@@ -0,0 +1,40 @@
+import { PassengerAccountOptionsService } from './passengerAccountOptions.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WorkingHoursService {
+  constructor(private http: HttpClient) {}
+
+  startCounting(driverId: number): Observable<WorkingHours> {
+    const startDto: WorkingHoursStartDTO = {
+        start: new Date()
+    };
+    return this.http.post<WorkingHours>(environment.apiHost + '/driver/' + driverId + '/working-hour', startDto);
+  }
+
+  endCounting(workingHoursId: number): Observable<WorkingHours> {
+    const endDto: WorkingHoursEndDTO = {
+        end: new Date()
+    };
+    return this.http.put<WorkingHours>(environment.apiHost + '/driver/working-hour/' + workingHoursId, endDto);
+  }
+}
+
+export interface WorkingHours {
+    id: number,
+    start: string,
+    end: string
+}
+
+export interface WorkingHoursStartDTO {
+    start: Date
+}
+
+export interface WorkingHoursEndDTO {
+    end: Date
+}
