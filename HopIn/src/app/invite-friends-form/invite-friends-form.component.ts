0f2046602aa094dd491f0f0dd3a10325bb16481d
tince250 <t.s.mihajlovic25@gmail.com>
2023-01-09 10:02:55 +0100

    added logic for adding/removing passengers from passengers array, before sending invites

--- HopIn/src/app/invite-friends-form/invite-friends-form.component.ts
+++ HopIn/src/app/invite-friends-form/invite-friends-form.component.ts
@@ -1,7 +1,10 @@
-import { RidePassenger } from './../services/routing.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SharedService } from './../shared/shared.service';
+import { PassengerService } from './../services/passenger.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { markFormControlsTouched } from '../validators/formGroupValidators';
+import { User } from '../services/user.service';
 
 @Component({
   selector: 'invite-friends-form',
@@ -10,27 +13,57 @@ import { markFormControlsTouched } from '../validators/formGroupValidators';
 })
 export class InviteFriendsFormComponent implements OnInit {
 
-  passengers: RidePassenger[] = [];
+  hasOthers: boolean = false;
+
+  passengers: User[] = [];
 
   inviteForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email])
   });
 
-  constructor() { }
+  constructor(private passengerService: PassengerService, private sharedService: SharedService) { }
 
   ngOnInit(): void {
     markFormControlsTouched(this.inviteForm);
   }
 
   addPassenger() {
-
+    this.passengerService.findByEmail(this.inviteForm.value.email!).subscribe({
+      next: (passenger) => {
+        if (!this.hasOthers) {
+          this.showLinkedFriends();
+        }
+        this.passengers.push(passenger);
+      }, 
+      error: (err: HttpErrorResponse) => {
+        if (err.status == 404)
+          this.sharedService.openSnack({
+            value: "Seems like there's no active passenger with that email.",
+            color: "back-red"
+          });
+        else 
+          this.sharedService.openSnack({
+            value: "An error occured while trying to fetch data.",
+            color: "back-red"
+          });
+      }
+    });
   }
 
-  removePassenger() {
+  showLinkedFriends() {
+    this.hasOthers = true;
+  }
 
+  removePassenger(i: number) {
+    this.passengers.splice(i, 1);
+    console.log(this.passengers);
   }
 
   orderRide() {
+
+  }
+
+  sendInvites() {
 
   }
 
