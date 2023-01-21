8d330fbdf80c27f6365a3f398fc498a4d579c984
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-21 22:04:17 +0100

    added methods in ride-history-component for communication with backend

--- HopIn/src/app/ride-history/ride-history.component.ts
+++ HopIn/src/app/ride-history/ride-history.component.ts
@@ -1,3 +1,6 @@
+import { AuthService } from './../services/auth.service';
+import { ReviewService, CompleteRideReviewDTO } from './../services/review.service';
+import { RideService, RideReturnedDTO } from './../services/ride.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
@@ -7,23 +10,72 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class RideHistoryComponent implements OnInit {
 
-  public form!: FormGroup;
-  rating: number;
+  _role: String = ''
+  _id: number = 0
+  rating = 4
+  rides!: RideReturnedDTO[]
+  ratings!: number[]
 
-  constructor(private fb: FormBuilder) {
-    this.rating = 0;
-    this.form = this.fb.group({
-      rating: ['', Validators.required],
-    })
+
+  constructor(private rideService: RideService,
+    private reviewService: ReviewService,
+    private authService: AuthService) {
+
    }
 
   ngOnInit(): void {
+    this.authService.getUser().subscribe((res) => {
+      this._role = res;
+      this._id = this.authService.getId();
+    })
+
+    this.getRides();
   }
 
-  ratingStyle = {
-    starsStyle: {'height' : '22px', 'width' : '22px'},
-    ratingStyle: {'font-size' : '18px'},
-    countStyle: {'font-size' : '14px'}
-}
+  getRides() {
+    this.rideService.getAll(this._id).subscribe({
+      next: (res) => {
+        this.rides = res.results
+        this.getRatings();
+        console.log(this.rides);
+        console.log(this.ratings);
+      },
+      error: (error: any) => {
+        console.log(error)
+      } 
+    });
+  }
+
+  getRatings() {
+    this.ratings = [];
+    for (let ride of this.rides) {
+      this.reviewService.getAll(ride.id).subscribe({
+        next: (res) => {
+          let sum = 0;
+          let counter = 0;
+          for(let pair of res) {
+            if (pair.vehicleReview != null) {
+              sum += pair.vehicleReview.rating
+              counter++;
+            }
+            if (pair.driverReview != null) {
+              sum += pair.driverReview.rating
+              counter++;
+            }
+          }
+          this.ratings.push(sum);
+          if (sum != 0) {
+            this.ratings.pop();
+            this.ratings.push(sum/counter);
+          }
+        },
+        error: (error: any) => {
+          //console.log(error)
+        } 
+      });
+    }
+    
+  }
+
 
 }
