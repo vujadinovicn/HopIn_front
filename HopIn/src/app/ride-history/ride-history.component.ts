500ebf155b7416eae93e0cca82dfb529ad86cb1f
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-22 01:08:54 +0100

    binded html and ts, so data is displayed dynamically, also added favorite option on cards, and loading true rating

--- HopIn/src/app/ride-history/ride-history.component.ts
+++ HopIn/src/app/ride-history/ride-history.component.ts
@@ -1,8 +1,12 @@
+import { FavouriteRoutesService } from './../favouriteRoutesService/favourite-routes.service';
 import { AuthService } from './../services/auth.service';
 import { ReviewService, CompleteRideReviewDTO } from './../services/review.service';
 import { RideService, RideReturnedDTO } from './../services/ride.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FavouriteRoute } from '../favourite-routes/favourite-routes.component';
+import { distinct } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'ride-history',
   templateUrl: './ride-history.component.html',
@@ -13,12 +17,23 @@ export class RideHistoryComponent implements OnInit {
   _role: String = ''
   _id: number = 0
   rating = 4
-  rides!: RideReturnedDTO[]
-  ratings!: number[]
+  rides: RideReturnedDTO[] = []
+  ratings: number[] = []
+  isfavoriteRoutes: boolean[] = [];
+  favoriteRoutes: FavouriteRoute[] = [];
+
+  emptyFavorite: FavouriteRoute = {
+    id: 0,
+    distance: 0,
+    departure: {address: '', longitude: 0, latitude: 0},
+    destination: {address: '', longitude: 0, latitude: 0},
+  }
 
 
   constructor(private rideService: RideService,
     private reviewService: ReviewService,
+    private favoriteRouteService: FavouriteRoutesService,
+    public snackBar: MatSnackBar,
     private authService: AuthService) {
 
    }
@@ -37,8 +52,7 @@ export class RideHistoryComponent implements OnInit {
       next: (res) => {
         this.rides = res.results
         this.getRatings();
-        console.log(this.rides);
-        console.log(this.ratings);
+        this.setFavorites();
       },
       error: (error: any) => {
         console.log(error)
@@ -48,7 +62,7 @@ export class RideHistoryComponent implements OnInit {
 
   getRatings() {
     this.ratings = [];
-    for (let ride of this.rides) {
+    for (let ride of this.rides.reverse()) {
       this.reviewService.getAll(ride.id).subscribe({
         next: (res) => {
           let sum = 0;
@@ -63,18 +77,78 @@ export class RideHistoryComponent implements OnInit {
               counter++;
             }
           }
-          this.ratings.push(sum);
+          this.ratings.push(ride.id);
           if (sum != 0) {
             this.ratings.pop();
             this.ratings.push(sum/counter);
           }
         },
         error: (error: any) => {
-          //console.log(error)
+          console.log(error)
         } 
       });
     }
-    
+  }
+
+  setFavorites() {
+    this.favoriteRouteService.getAll(this._id).subscribe({
+      next: (res) => {
+        this.isfavoriteRoutes = [];
+        this.favoriteRoutes = [];
+        for(let ride of this.rides.reverse()) {
+          this.isfavoriteRoutes.push(false);
+          this.favoriteRoutes.push(this.emptyFavorite);
+          for (let route of res) {
+            if (route.departure.latitude == ride.locations[0].departure.latitude && route.departure.longitude == ride.locations[0].departure.longitude) {
+              if (route.destination.latitude == ride.locations[0].destination.latitude && route.destination.longitude == ride.locations[0].destination.longitude) {
+                this.isfavoriteRoutes.pop();
+                this.favoriteRoutes.pop();
+                this.isfavoriteRoutes.push(true);
+                this.favoriteRoutes.push(route);
+              }
+            } 
+          }
+        }
+        console.log(this.favoriteRoutes);
+      },
+      error: (error: any) => {
+        this.isfavoriteRoutes = new Array(this.rides.length).fill(false);
+        this.favoriteRoutes = new Array(this.rides.length).fill(this.emptyFavorite);
+      } 
+    });
+  }
+
+
+  removeRoute(index: number): void {
+    this.favoriteRouteService.removeFavoriteRoute(this.favoriteRoutes[index].id, this._id).subscribe({
+      next: (res) => {
+        this.getRides();
+      },
+      error: (error: any) => {
+        this.snackBar.open("Sorry, server is currenty unavailable!", "", {
+          duration: 2000,
+       });
+      } 
+    });
+  }
+
+  returnRoute(index: number): void {
+    let route: FavouriteRoute = {
+      id: 0,
+      distance: this.rides[index].distance,
+      departure: this.rides[index].locations[0].departure,
+      destination: this.rides[index].locations[0].destination
+    }
+    this.favoriteRouteService.addNewFavoriteRoute(this._id, route).subscribe({
+      next: (res) => {
+        this.getRides();
+      },
+      error: (error: any) => {
+        this.snackBar.open("Sorry, server is currenty unavailable!", "", {
+          duration: 2000,
+       });
+      } 
+    });
   }
 
 
