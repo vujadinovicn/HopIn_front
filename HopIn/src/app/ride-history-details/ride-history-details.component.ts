caf4d3eb5dc37709618e117211223c2a06cb29bf
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-23 20:26:50 +0100

    created ride-history-details and ride-info-card component without design

---
+++ HopIn/src/app/ride-history-details/ride-history-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ride-history-details',
+  templateUrl: './ride-history-details.component.html',
+  styleUrls: ['./ride-history-details.component.css']
+})
+export class RideHistoryDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ HopIn/src/app/ride-info-card/ride-info-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ride-info-card',
+  templateUrl: './ride-info-card.component.html',
+  styleUrls: ['./ride-info-card.component.css']
+})
+export class RideInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
