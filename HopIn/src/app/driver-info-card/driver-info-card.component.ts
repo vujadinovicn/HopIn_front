57d3dcae950bc93b50ae51038215cbb288677c27
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-24 00:18:30 +0100

    added dynamically data loading for driver-info-card component

--- HopIn/src/app/driver-info-card/driver-info-card.component.ts
+++ HopIn/src/app/driver-info-card/driver-info-card.component.ts
@@ -1,4 +1,6 @@
+import { Driver, User, UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { RideReturnedDTO, RideService } from '../services/ride.service';
 
 @Component({
   selector: 'app-driver-info-card',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverInfoCardComponent implements OnInit {
 
-  constructor() { }
+  ride!: RideReturnedDTO;
+  driver!: Driver;
+  url : String = "../../assets/images/profile-placeholder.png";
+
+  onFileSelect(event: any){
+    if (event.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload=(e: any)=>{
+        this.url = reader.result as string;
+      }
+    }
+  }
+
+  constructor(private rideService: RideService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.rideService.getRide().subscribe((res) => {
+      this.ride = res;
+      this.userService.getByDriverId(res.driver.id).subscribe((d) => {
+        this.driver = d;
+        if (d.profilePicture != null) {this.url = d.profilePicture;}
+      })
+    });
   }
 
 }
