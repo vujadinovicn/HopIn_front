297e5034c09cf1d55793b8853761f9e519d751f8
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-23 23:23:03 +0100

    created initial design for driver-card-info component

---
+++ HopIn/src/app/driver-info-card/driver-info-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-info-card',
+  templateUrl: './driver-info-card.component.html',
+  styleUrls: ['./driver-info-card.component.css']
+})
+export class DriverInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
