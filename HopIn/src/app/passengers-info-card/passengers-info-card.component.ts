362a74d0a9e2bbae8b450192fb6ee0fba89c3164
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-24 15:23:12 +0100

    finished dynamically loading data in passengers-info-card component for passed ride

--- HopIn/src/app/passengers-info-card/passengers-info-card.component.ts
+++ HopIn/src/app/passengers-info-card/passengers-info-card.component.ts
@@ -1,4 +1,6 @@
+import { RideReturnedDTO, RideService } from './../services/ride.service';
 import { Component, OnInit } from '@angular/core';
+import { User, UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-passengers-info-card',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PassengersInfoCardComponent implements OnInit {
 
-  constructor() { }
+  ride!: RideReturnedDTO;
+  passengers: User[] = [];
+  urls: String[] = [];
+
+  constructor(private rideService: RideService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.rideService.getRide().subscribe((res) => {
+      this.ride = res;
+      this.urls = [];
+      for (let pass of this.ride.passengers) {
+        this.userService.getByPassengerId(pass.id).subscribe((p) => {
+          this.passengers.push(p);
+          if (p.profilePicture == null) {
+            this.urls.push("../../assets/images/profile-placeholder.png");
+          } else {
+            this.urls.push(p.profilePicture);
+          }
+        })
+      }
+    })
   }
 
 }
