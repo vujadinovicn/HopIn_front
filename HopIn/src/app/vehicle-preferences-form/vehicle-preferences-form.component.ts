7498ede59ec8e750705d48950d776cb9a33ecdce
tince250 <t.s.mihajlovic25@gmail.com>
2023-01-27 16:19:06 +0100

    enabled loading screen opening when ordering ride from vehicle preferences comp

--- HopIn/src/app/vehicle-preferences-form/vehicle-preferences-form.component.ts
+++ HopIn/src/app/vehicle-preferences-form/vehicle-preferences-form.component.ts
@@ -1,3 +1,5 @@
+import { LoadingDialogComponent } from './../loading-dialog/loading-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from './../services/auth.service';
 import { RoutingService } from './../services/routing.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
@@ -17,7 +19,9 @@ export class VehiclePreferencesFormComponent implements OnInit {
   isBabyTransport : boolean = false;
   isPetTransport: boolean = false;
 
-  constructor(private routingService: RoutingService, private authService: AuthService) { }
+  constructor(private routingService: RoutingService, 
+              private authService: AuthService,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
@@ -39,6 +43,12 @@ export class VehiclePreferencesFormComponent implements OnInit {
     this.updateValues();
     this.routingService.setDefaultUser();
     this.routingService.findRoute();
+    this.routingService.receivedRoute().subscribe((res) => {
+      this.dialog.open(LoadingDialogComponent, {
+        data: {userId: this.authService.getId()}
+      });
+    });
+    
   }
 
   private updateValues() {
