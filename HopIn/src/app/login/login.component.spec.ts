a20b3abe8725e23f5beaef2c93f4719ec59fc214
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-02-07 19:01:47 +0100

    test set up

--- HopIn/src/app/login/login.component.spec.ts
+++ HopIn/src/app/login/login.component.spec.ts
@@ -1,23 +1,48 @@
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { AuthService } from './../services/auth.service';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { SocketService } from '../services/socket.service';
+import { RedirectionService } from '../services/redirection.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Overlay } from '@angular/cdk/overlay';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ HttpClientModule, FormsModule, MatDialogModule, ReactiveFormsModule],
+      providers: [AuthService, Overlay, MatDialog,
+      MatSnackBar]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    de = fixture.debugElement.query(By.css('.title'));
+    el = de.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should title be present', () => {
+    expect(el.innerHTML).toEqual('Welcome back');
+  });
+
+
 });
